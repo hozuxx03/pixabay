34f63a91423dade427d1487d43d6e17b25717e16
hozuxx03 <y.m.m.s.xxx@gmail.com>
2023-06-12 20:07:52 +0900

    first

---
+++ src/components/image.List.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Masonry from "react-masonry-component";
+
+import "../styles/ImageList.css";
+
+
+const ImageList = (props)=>{
+    const images = props.images.map((image)=>{
+        return(
+            <a
+            href={image.pageURL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className=" ui medium image"
+            >
+            <img src={image.webformatURL} alt={image.tags}/>
+            </a>
+        );
+        });
+    return(<Masonry className="ImageList">{images}</Masonry>
+    );
+};
+
+export default ImageList;
